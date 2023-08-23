a29f30ad7b95213f4eaa59b8e0b4123052792716
rendyfrasya <rendyfrasya456@gmail.com>
2023-08-23 18:26:19 +0700

    Second commit of API TA V2

--- routes/route.js
+++ routes/route.js
@@ -8,7 +8,7 @@ const format = "YYYY-MM-DD HH:mm:ss";
 
 
 router.get("/getdata", (req, res) => {
-  const sql = "SELECT target_ppm,target_pH,DATE_FORMAT(waktu_siram, '%H:%i') AS waktu_siram, DATE_FORMAT(waktu_pH, '%H:%i') AS waktu_pH, DATE_FORMAT(waktu_ppm, '%H:%i') AS waktu_ppm FROM parameter_val";
+  const sql = "SELECT target_ppm,target_pH,DATE_FORMAT(waktu_siramPagi, '%H:%i') AS waktu_siramPagi,DATE_FORMAT(waktu_siramSore, '%H:%i') AS waktu_siramSore, DATE_FORMAT(waktu_pH, '%H:%i') AS waktu_pH, DATE_FORMAT(waktu_ppm, '%H:%i') AS waktu_ppm FROM parameter_val";
   db.query(sql, (err, fields) => {
     if (err) response(500, "INVALID", "ERROR", res);
     response(200, fields, "ini message", res);
@@ -25,7 +25,7 @@ router.get("/gettarget", (req, res) => {
 });
 
 router.get("/getwaktupompa", (req, res) => {
-  const sql = "SELECT waktu_siram, waktu_pH, waktu_ppm FROM parameter_val ";
+  const sql = "SELECT waktu_siramPagi,waktu_siramSore, waktu_pH, waktu_ppm FROM parameter_val ";
   db.query(sql, (err, fields) => {
     if (err) response(500, "INVALID", "ERROR", res);
     response(200, fields, "ini message", res);
@@ -52,67 +52,12 @@ router.post("/updatetarget", (req, res) => {
 
 router.post("/updatewaktu", (req, res) => {
   let formData = {
-    waktu_siram: req.query.waktu_siram,
+    waktu_siramPagi: req.query.waktu_siramPagi,
+    waktu_siramSore: req.query.waktu_siramSore,
     waktu_ppm: req.query.waktu_ppm,
     waktu_pH: req.query.waktu_pH,
   };
   const sql2 = `UPDATE parameter_val SET ? WHERE id = 1 `;
-  db.query(sql2, formData, (err, fields) => {
-    if (err) response(500, "INVALID", "ERROR", res);
-    if (fields?.affectedRows) {
-      const data = {
-        isSuccess: fields.affectedRows,
-        id: fields.message,
-      };
-      response(200, data, "Post Data Success", res);
-    }
-  });
-});
-
-router.get("/putdata", (req, res) => {
-  let formData = {
-    suhu: req.query.suhu,
-    suhuAir: req.query.suhuDS,
-    kelembaban : req.query.kelembaban,
-    ppm : req.query.ppm
-  };
-  const sql1 = `INSERT INTO data_sensors SET ?`;
-  db.query(sql1, formData, (err, fields) => {
-    if (err) response(500, "INVALID", "ERROR", res);
-    console.log(err)
-    if (fields?.affectedRows) {
-      const data = {
-        isSuccess: fields.affectedRows,
-        id: fields.insertId,
-      };
-    }
-  });
-  const sql2 = `UPDATE logs_sensors SET ? WHERE id = 1`;
-  db.query(sql2, formData, (err, fields) => {
-    if (err) response(500, "INVALID", "ERROR", res);
-    if (fields?.affectedRows) {
-      const data = {
-        isSuccess: fields.affectedRows,
-        id: fields.message,
-      };
-    }
-  });
-  const sql4 = "SELECT * FROM response_toarduino";
-  db.query(sql4, (err, fields) => {
-    if (err) response(500, "INVALID", "ERROR", res);
-    return response(200, fields, "Arduino Data Sucess", res);
-  });
-});
-
-router.get("/insertdt", (req, res) => {
-  let formData = {
-    suhu: req.query.suhu,
-    suhuAir: req.query.suhuDS,
-    kelembaban : req.query.kelembaban,
-    // kelembaban_med : '0',
-    ppm : req.query.ppm
-  };
-  const sql2 = `INSERT INTO data_sensors SET ?`;
   db.query(sql2, formData, (err, fields) => {
     if (err) response(500, "INVALID", "ERROR", res);
     if (fields?.affectedRows) {
